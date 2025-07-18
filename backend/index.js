13e4829c96ff9d31d6df8de461e9c7ed663daf5c
sidhdhakal <siddharthadhakal54@gmail.com>
2025-07-18 22:20:28 +0545

    Adding dokcer file

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors()); 
 app.use(express.json({ limit: "10mb" }));
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 const contactSchema = mongoose.Schema({
   name: String, 
   email: String,
